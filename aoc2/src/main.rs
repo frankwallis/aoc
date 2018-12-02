b8ea41c3a8ba53e9307ebeb52c44b09804ac6760
Frank Wallis <fwallis@outlook.com>
2018-12-02 22:02:02 +0000

    tweaks

--- aoc2/src/main.rs
+++ aoc2/src/main.rs
@@ -20,23 +20,17 @@ fn main() -> Result<()> {
 }
 
 fn solve1(input: &[String]) {
-    let count2 = get_count(&input, 2);
-    let count3 = get_count(&input, 3);
+    let count2 = get_count(&input, &2);
+    let count3 = get_count(&input, &3);
     println!("{}", count2 * count3);
 }
 
-fn get_count(vector: &[String], freq: i32) -> i32 {
-    vector.iter().fold(0, |result, str| {
-        if has_frequency(str, &freq) {
-            result + 1
-        } else {
-            result
-        }
-    })
+fn get_count(input: &[String], freq: &u32) -> usize {
+    input.iter().filter(|str| has_frequency(str, freq)).count()
 }
 
-fn has_frequency(str: &String, frequency: &i32) -> bool {
-    let mut lookup: HashMap<char, i32> = HashMap::new();
+fn has_frequency(str: &String, frequency: &u32) -> bool {
+    let mut lookup: HashMap<char, u32> = HashMap::new();
 
     for chr in str.chars() {
         let count = match lookup.get(&chr) {
@@ -53,40 +47,27 @@ fn has_frequency(str: &String, frequency: &i32) -> bool {
 fn solve2(input: &[String]) {
     let mut remaining = input;
 
-    let answer = loop {
+    let result = loop {
         match remaining.split_first() {
-            Some((str1, rest)) => {
-                let found = rest.iter().fold(None, |result, str2| match result {
-                    None => {
-                        if get_distance(str1, str2) == 1 {
-                            Some(get_common_letters(str1, str2))
-                        } else {
-                            None
-                        }
-                    }
-                    Some(letters) => Some(letters),
-                });
-
-                match found {
-                    Some(letters) => break Some(letters),
-                    None => remaining = rest,
-                }
-            }
+            Some((str1, rest)) => match rest.iter().find(|str2| get_distance(str1, str2) == 1) {
+                Some(str2) => break Some(get_common_letters(str1, str2)),
+                None => remaining = rest,
+            },
             None => break None,
         }
     };
 
-    match answer {
+    match result {
         Some(letters) => println!("{}", letters),
         None => println!("No results"),
     }
 }
 
-fn get_distance(str1: &String, str2: &String) -> i32 {
-    str1.chars().zip(str2.chars()).fold(
-        0,
-        |result, (chr1, chr2)| if chr1 == chr2 { result } else { result + 1 },
-    )
+fn get_distance(str1: &String, str2: &String) -> usize {
+    str1.chars()
+        .zip(str2.chars())
+        .filter(|(chr1, chr2)| chr1 != chr2)
+        .count()
 }
 
 fn get_common_letters(str1: &String, str2: &String) -> String {
